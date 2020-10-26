117e04238163136666864a3138aee6d3a6589fd0
SHB910317 <hernandezbarrero@gmail.com>
2020-10-26 11:51:55 -0500

    subscripción al servicio de httpClient
    Se creó el componente menubreaksfast, el servicio dtaservice para usar httpclient y obtener la datel archivo Json usando la url de github raw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenubreakfastComponent } from './menubreakfast/menubreakfast.component';
+//import { DataService } from './data.service';
+import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenubreakfastComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    //DataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
