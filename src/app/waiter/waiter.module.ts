3bbd2ffdc4e26d61adfb0fd8d386c3885974625a
SHB910317 <hernandezbarrero@gmail.com>
2020-11-03 14:01:18 -0500

    Lazy Loading
    Creación del modulo waiter con su routing y estamos tratando de crear las rutas hijas. Enrutar el componente del modulo

--- src/app/waiter/waiter.module.ts
+++ src/app/waiter/waiter.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { createOrderComponent } from './createOrder/createOrder.component';
-
+import { WaiterRoutingModule } from './waiter-routing.module';
+import {createOrderComponent} from './createOrder/createOrder.component'
+  
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    WaiterRoutingModule,
     createOrderComponent
   ]
 })
