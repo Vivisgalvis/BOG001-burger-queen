e051505e440aa1b81f7a80b834792bb88e209b01
vivisGalvis <vivigalvis.89@gmail.com>
2020-11-05 09:19:06 -0500

    Enrutamiento de boton de Waiter

--- src/app/waiter/waiter.module.ts
+++ src/app/waiter/waiter.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { WaiterRoutingModule } from './waiter-routing.module';
+import { SelectTableComponent } from './select-table/select-table.component';
+import { CreateOrderComponent } from './create-order/create-order.component';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SelectTableComponent, CreateOrderComponent],
   imports: [
     CommonModule,
     WaiterRoutingModule
